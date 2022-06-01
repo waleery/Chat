179315a72c13fc1154f7e19c4aa65ae12c07f1ae
waleery <walenda1997@gmail.com>
2022-06-01 11:11:22 +0200

    added Websocket config, message model

---
+++ chatserver/src/main/java/com/example/chatserver/controller/ChatController.java
@@ -0,0 +1,4 @@
+package com.example.chatserver.controller;
+
+public class ChatController {
+}
---
+++ chatserver/src/main/java/com/example/chatserver/controller/model/Status.java
@@ -0,0 +1,7 @@
+package com.example.chatserver.controller.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
