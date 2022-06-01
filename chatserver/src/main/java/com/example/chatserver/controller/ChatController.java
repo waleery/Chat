8ce9cae740d62679a47d7bc4d5556e05f1d5dadf
waleery <walenda1997@gmail.com>
2022-06-01 11:22:38 +0200

    added chatController

--- chatserver/src/main/java/com/example/chatserver/controller/ChatController.java
+++ chatserver/src/main/java/com/example/chatserver/controller/ChatController.java
@@ -1,4 +1,29 @@
 package com.example.chatserver.controller;
 
+
+import com.example.chatserver.controller.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
 public class ChatController {
+
+    @Autowired
+    public SimpMessagingTemplate simpMessagingTemplate;
+
+
+
+    @MessageMapping("/message")  //  /app/message
+    @SendTo("/chateroom/public")
+    public Message receivePublicMessage(@Payload Message message){
+        return message;
+    }
+
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message){
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message);//  /user/patryk/private
+        return message;
+    }
 }
